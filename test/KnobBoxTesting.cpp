46389d30f51f1614d6bd11f87e0cb7b8c21e0edb
tbai9 <tbai9@wisc.edu>
2025-04-17 21:13:24 -0500

    modified:   test/KnobBoxTesting.cpp

--- test/KnobBoxTesting.cpp
+++ test/KnobBoxTesting.cpp
@@ -15,7 +15,7 @@ Adafruit_ADS1115 ads;
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 //Timer for read ADC Value
-auto timer = timer_create_default();
+Timer<2, millis> timer;
 
 // Channel assignment:
 //   A0 -> Voltage Monitor from Bertan 
@@ -30,85 +30,28 @@ auto timer = timer_create_default();
 #define SET_BERTAN_20KV 50
 #define SET_MATSUSADA_1KV 52
 
-int voltage_multiplier = 0;
+float voltage_multiplier = 0;
 float current_multiplier = 0;
 
 // For convenience, pick an ADS1115 data rate that easily exceeds 2.5 samples/s.
 // For instance, the default (128 SPS) is plenty fast for a 400 ms loop.
 
 //Timer callback
-uint16_t potRaw = 0;
-uint16_t vmonRaw = 0;
-uint16_t imonRaw = 0;
+int16_t potRaw = 0;
+int16_t vmonRaw = 0;
+int16_t imonRaw = 0;
+
+float hvProgram_V;
+float measuredHV_V;
+float measuredI_mA;
+
 
 bool read_value(void *) { //Callback to read ADC
   // Read the four ADS1115 channels in single‐ended mode:
   potRaw       = ads.readADC_SingleEnded(CH_POT);
   vmonRaw      = ads.readADC_SingleEnded(CH_VMON);
   imonRaw      = ads.readADC_SingleEnded(CH_IMON);
-  return true; // repeat? true
-}
 
-void setup()
-{
-  Serial.begin(9600);
-  Serial.println("Bertan 205B-03R Monitoring with ADS1115 Test");
-
-  // Initialize the I²C bus, ADS1115, and set sample rate/gain if desired
-  Wire.begin();   
-  ads.begin();
-  // Optionally pick a data rate:
-  //   ads.setDataRate(RATE_ADS1115_128SPS); // default
-  // or  ads.setDataRate(RATE_ADS1115_250SPS);
-  //
-  // Optionally pick a gain (depends on your 0–5 V signals):
-  //   ads.setGain(GAIN_ONE);    // ±4.096 V range (good if your signals never exceed ~4 V)
-  //   ads.setGain(GAIN_TWO);    // ±2.048 V
-  //   ads.setGain(GAIN_FOUR);   // ±1.024 V
-  // etc. The default is GAIN_TWOTHIRDS (±6.144 V), fine for 0–5 V.
-
-  //Init pin for checking
-  pinMode(SET_BERTAN_3KV, INPUT_PULLUP);
-  pinMode(SET_BERTAN_20KV, INPUT_PULLUP);
-  pinMode(SET_MATSUSADA_1KV, INPUT_PULLUP);
-  
-  // Initialize the LCD
-  lcd.init();
-  lcd.backlight();
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  if(!digitalRead(SET_BERTAN_3KV)){         //Pin 48 pull down
-    lcd.print("Bertan 3kV Setup");
-    lcd.setCursor(0, 1);
-    lcd.print("20kV Setup");
-    voltage_multiplier = 3000;
-    current_multiplier = 10.0;
-  }else if(!digitalRead(SET_BERTAN_20KV)){  //Pin 50 pull down
-    lcd.print("Bertan");
-    lcd.setCursor(0, 1);
-    lcd.print("20kV Setup");
-    voltage_multiplier = 20000;
-    current_multiplier = 1.0;
-  }else if(!digitalRead(SET_MATSUSADA_1KV)){//Pin 52 pull down
-    lcd.print("Matsusada");
-    lcd.setCursor(0, 1);
-    lcd.print("1kV Setup");
-    voltage_multiplier = 1000;
-    current_multiplier = 1.0;
-  }else{                                    //Nothing connencted
-    lcd.print("Error");
-    lcd.setCursor(0,1);
-    lcd.print("No mode set!");
-    while (true){} //Halt everything
-  }
-  delay(1000);  //Wait a little bit for everything ready
-  lcd.clear();
-
-  timer.every(200, read_value); //Setup timer callback
-}
-
-void loop()
-{
   // Convert from raw ADC reading to voltage at each ADS input pin.
   // By default, the Adafruit library uses ±6.144 V as the full‐scale range,
   // which yields ~0.1875 mV/LSB. If you used setGain(GAIN_ONE) or others,
@@ -128,10 +71,113 @@ void loop()
   // The polarity pin is open-collector; if you’ve got a pull-up to 5 V, reading near 0 means
   // positive polarity, near 5 means negative. You can threshold it to figure out text labels.
 
-  float hvProgram_V = (potVolts / 5.0) * voltage_multiplier;     // example for 3 kV supply
-  float measuredHV_V = (vmonVolts / 5.0) * voltage_multiplier;   // for 3 kV supply
-  float measuredI_mA  = (imonVolts / 5.0) * current_multiplier;  // for 10 mA max rating
+  hvProgram_V = (potVolts / 5.0) * voltage_multiplier;     // example for 3 kV supply
+  measuredHV_V = (vmonVolts / 5.0) * voltage_multiplier;   // for 3 kV supply
+  measuredI_mA  = (imonVolts / 5.0) * current_multiplier;  // for 10 mA max rating
 
+  return true; // repeat? true
+}
+
+char buffer[16];
+char vbuffer[16];
+String current;
+
+bool display_value(void *) { //Callback to Display value
+  // Display two readings on each of the two LCD rows
+  // Row 1: HV setpoint (kV) and measured HV
+  sprintf(buffer, "Vs:%5dV ", int(hvProgram_V));
+
+  lcd.setCursor(0, 0);
+  lcd.print(buffer);
+  lcd.setCursor(10, 0);
+  lcd.print("I:");
+
+  sprintf(vbuffer, "V:%5dV ", int(measuredHV_V));
+  current = String(measuredI_mA,1) + "mA  ";
+  // Row 2: measured current (mA) and polarity
+  lcd.setCursor(0, 1);
+  lcd.print(vbuffer);
+  lcd.setCursor(10, 1);
+  lcd.print(current);
+  
+  return true; // repeat? true
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("High voltage power supply Monitoring with ADS1115 Test");
+
+  // Initialize the I²C bus, ADS1115, and set sample rate/gain if desired
+  Wire.begin();   
+  ads.begin();
+  // Optionally pick a data rate:
+  //   ads.setDataRate(RATE_ADS1115_128SPS); // default
+  // or  ads.setDataRate(RATE_ADS1115_250SPS);
+  //
+  // Optionally pick a gain (depends on your 0–5 V signals):
+  //   ads.setGain(GAIN_ONE);    // ±4.096 V range (good if your signals never exceed ~4 V)
+  //   ads.setGain(GAIN_TWO);    // ±2.048 V
+  //   ads.setGain(GAIN_FOUR);   // ±1.024 V
+  // etc. The default is GAIN_TWOTHIRDS (±6.144 V), fine for 0–5 V.
+
+  //Init pin for checking
+  pinMode(SET_BERTAN_3KV, INPUT_PULLUP);
+  pinMode(SET_BERTAN_20KV, INPUT_PULLUP);
+  pinMode(SET_MATSUSADA_1KV, INPUT_PULLUP);
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  // Initialize the LCD
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  if(!digitalRead(SET_BERTAN_3KV)){         //Pin 48 pull down
+    lcd.print("Bertan 3kV Setup");
+    lcd.setCursor(0, 1);
+    lcd.print("20kV Setup");
+    voltage_multiplier = 3000.0;
+    current_multiplier = 10.0;
+  }else if(!digitalRead(SET_BERTAN_20KV)){  //Pin 50 pull down
+    lcd.print("Bertan");
+    lcd.setCursor(0, 1);
+    lcd.print("20kV Setup");
+    voltage_multiplier = 20000.0;
+    current_multiplier = 1.0;
+  }else if(!digitalRead(SET_MATSUSADA_1KV)){//Pin 52 pull down
+    lcd.print("Matsusada");
+    lcd.setCursor(0, 1);
+    lcd.print("1kV Setup");
+    voltage_multiplier = 1000.0;
+    current_multiplier = 30.0;
+  }else{                                    //Nothing connencted
+    lcd.print("Error");
+    lcd.setCursor(0,1);
+    lcd.print("No mode set!");
+    while (true){//Halt everything, indicate error through LED_BUILTIN
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(250);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(250);
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(250);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(750);
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(750);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(250);
+    } 
+  }
+  delay(1000);  //Wait a little bit for everything ready
+  lcd.clear();
+
+  timer.every(200, read_value); //Setup timer callback
+  timer.every(150, display_value); //Setup timer callback
+}
+
+void loop()
+{
   // Print results to Serial
   Serial.print("Set: ");
   Serial.print(hvProgram_V, 0);
@@ -140,27 +186,6 @@ void loop()
   Serial.print(" V,  I: ");
   Serial.print(measuredI_mA, 2);
   Serial.print(" mA");
-
-  // Display two readings on each of the two LCD rows
-  // Row 1: HV setpoint (kV) and measured HV
-  char buffer[10];
-  sprintf(buffer, "%05d", int(hvProgram_V));
-  String fvar = "Vs:" + String(buffer) + "V ";
-  
-  lcd.setCursor(0, 0);
-  lcd.print(fvar);
-  lcd.setCursor(10, 0);
-  lcd.print("I:");
-
-  char vbuffer[10];
-  sprintf(vbuffer, "%05d", int(measuredHV_V));
-  String secondRow = "V:" + String(vbuffer) + "V ";
-  String current = String(measuredI_mA,1) + "mA     ";
-  // Row 2: measured current (mA) and polarity
-  lcd.setCursor(0, 1);
-  lcd.print(secondRow);
-  lcd.setCursor(10, 1);
-  lcd.print(current);
 
   timer.tick();
 }
